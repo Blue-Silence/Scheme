e04a87e13f080df6adefbf701a915569e7f2d0ec
Blue-Silence <Blue-Silence@users.noreply.github.com>
2018-10-02 21:57:02 +0800

    ~

---
+++ Main/Calculator/Else.rkt
@@ -0,0 +1,22 @@
+#lang racket
+(define (get-opt name target)
+  (define list (cdr target))
+  (define (get name list)
+    (if (eq? (car (car list)) name)
+        (cdr (car list))
+        (get name (cdr list))))
+  (get name list))
+
+(define (member-new? name target)
+  (define list (cdr target))
+  (define (? name list)
+    (cond ((null? list) #f)
+          ((eq? (car (car list)) name) #t)
+          (else (? name (cdr list)))))
+        
+  (? name list))
+
+(define (1st-opt list)
+  (car (cdr list)))
+
+        
