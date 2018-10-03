ec18b544494bbd04905bd3c3aa376c920483c59f
Blue-Silence <Blue-Silence@users.noreply.github.com>
2018-10-03 09:31:38 +0800

    ~

--- Main/Calculator/Parts/List.rkt
+++ Main/Calculator/Parts/List.rkt
@@ -1,12 +1,12 @@
 #lang racket
-(define (add order pair List)  ;;insert pairs into list.
+(define (add level pair List)  ;;insert pairs into list.
   (define tar-now (lambda () (car List)))
   (if (null? List)
-      (cons (cons order (list pair)) '())
-      (cond ((= (car (tar-now)) order) (cons (cons order (cons pair (cdr (tar-now)))) (cdr List)))
-            ((< order (car (tar-now))) (cons (cons order (List pair)) (cdr List)))
+      (cons (cons level (list pair)) '())
+      (cond ((= (car (tar-now)) level) (cons (cons level (cons pair (cdr (tar-now)))) (cdr List)))
+            ((< level (car (tar-now))) (cons (cons level (List pair)) (cdr List)))
             (else
-             (cons (car List) (add order pair (cdr List)))))))
+             (cons (car List) (add level pair (cdr List)))))))
 
 ;;Pair-example:(cons name proc)
 ;;Use-example:(add 1 (cons '+ +) list)
